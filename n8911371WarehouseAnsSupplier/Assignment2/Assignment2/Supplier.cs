b7b778108b71b7a2b7524a8f13f95bc8c3e44169
Lesterffx <lester0529ffx@gmail.com>
2014-10-13 16:04:09 +1000

    Warehouse and Supplier code
    1 st version

---
+++ n8911371WarehouseAnsSupplier/Assignment2/Assignment2/Supplier.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace Assignment2
+{
+    [DataContract]
+    public class Supplier
+    {
+        [DataMember]
+        public String componentNo;
+        [DataMember]
+        public String componentName;
+        [DataMember]
+        public int cost;
+        [DataMember]
+        public int amount;
+
+        public Supplier() { }
+
+        public Supplier(String componentNo, String componentName, int cost, int amount)
+        {
+            this.componentNo = componentNo;
+            this.componentName = componentName;
+            this.cost = cost;
+            this.amount = amount;
+        }
+    }
+}
