729ccf19487dacc90fc6cea5bd346334ae0fdef3
Lesterffx <lester0529ffx@gmail.com>
2014-10-14 19:06:30 +1000

    new Warehouse and Supplier service package name

---
+++ n8911371SupplierService/n8911371SupplierService/ISupplierService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace n8911371SupplierService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "ISupplierService" in both code and config file together.
+    [ServiceContract]
+    public interface ISupplierService
+    {
+        [OperationContract]
+        bool IsExistComponentNo(string componentNo);
+
+        [OperationContract]
+        string GetComponentName(String componentNo);
+
+        [OperationContract]
+        int GetCost(String componentNo);
+
+        [OperationContract]
+        int GetAmount(String componentNo);
+
+        [OperationContract]
+        bool CheckAmount(string componentNo, int exceptOrderAmount);
+
+        [OperationContract]
+        bool OrderAmount(string componentNo, int orderAmount);
+    }
+}
---
+++ n8911371WarehouseService/n8911371WarehouseService/IWarehouseService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace n8911371WarehouseService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IWarehouseService" in both code and config file together.
+    [ServiceContract]
+    public interface IWarehouseService
+    {
+        [OperationContract]
+        bool IsExistComponentNo(string componentNo);
+
+        [OperationContract]
+        string GetComponentName(String componentNo);
+
+        [OperationContract]
+        int GetCost(String componentNo);
+
+        [OperationContract]
+        int GetAmount(String componentNo);
+
+        [OperationContract]
+        Warehouse GetComponent(String componentNo);
+
+        [OperationContract]
+        bool CheckAmount(string componentNo, int exceptUsedAmount);
+
+        [OperationContract]
+        bool ConsumedAmount(string componentNo, int exceptConsumedAmount);
+
+        [OperationContract]
+        bool UpdatedAmount(string componentNo, int updatedAmount);
+    }
+}
