466445b07a100f24f07326dbd7901a8ffff60266
Zahran AlAbri <zzalabri@gmail.com>
2014-10-24 16:26:17 +1000

    garage

---
+++ n8964955-GarageWS/n8964955-GarageWS/In8964955-GarageWS.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace n8964955_GarageWS {
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "In8964955_GarageWS" in both code and config file together.
+    [ServiceContract]
+    public interface In8964955_GarageWS {
+        [OperationContract]
+        void setAppointment(string plateNo);
+
+        [OperationContract]
+        string getAppointment(string plateNo);
+
+        [OperationContract]
+        int getCost(string carModel);
+    }
+}
---
+++ n8964955-GarageWS/n8964955-GarageWS/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("n8964955-GarageWS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("n8964955-GarageWS")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b5d7eafb-cd39-4eb7-80b8-c155f6c1197f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
